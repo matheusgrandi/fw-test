31667d4007e993b9185f967fb3e36bcbb37e20b3
matheusgrandi <matheus@matheusgrandi.com.br>
2024-05-19 20:31:11 -0300

    feat: theme setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import createDefaultTheme from './theme/index.ts'
+import { ThemeProvider } from '@mui/material'
+
+const defaultTheme = createDefaultTheme()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ export default {
   theme: {
     extend: {
       animation: {},
+      colors: {
+        primary: '#FDE047',
+        secondary: '#374151',
+        tertiary: '#1F2937',
+        background: '#F3F4F6',
+        black: '#000000',
+        'gray-300': '#F3F4F6',
+        'gray-700': '#374151',
+        'gray-900': '#1F2937',
+        yellow: '#FDE047',
+        white: '#FFFFFF',
+      },
     },
   },
   plugins: [],

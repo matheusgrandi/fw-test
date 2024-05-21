5c68b9db8be45f6c3d7312d9aec8a638c23dde6b
matheusgrandi <matheus@matheusgrandi.com.br>
2024-05-21 15:18:07 -0300

    feat: signup mobile improvements

--- src/components/organisms/signup/SignUpCard.tsx
+++ src/components/organisms/signup/SignUpCard.tsx
@@ -11,11 +11,11 @@ const SignUpCard: React.FC = () => {
   return (
     <Box className='h-full max-w-[60rem]'>
       <Box className='flex items-center justify-center h-5/6 flex-col px-[calc(6.25rem-1.5rem)] gap-8'>
-        <Typography variant='h2' gutterBottom>
+        <Typography variant='h2' gutterBottom className='self-end'>
           Fieldwork is the leading sales tool for your HVAC business.
         </Typography>
-        <Box className='flex gap-8 pr-[6.25rem]'>
-          <Box className='flex gap-6 flex-col'>
+        <Box className='flex gap-8 pr-[6.25rem] max-md:flex-col max-md: pr-[0]'>
+          <Box className='flex gap-6 flex-col w-[19rem] max-md:10'>
             <Typography variant='h6' gutterBottom>
               For Business Owners
             </Typography>
@@ -40,7 +40,7 @@ const SignUpCard: React.FC = () => {
               transparency.
             </Typography>
           </Box>
-          <Box className='flex gap-6 flex-col'>
+          <Box className='flex gap-6 flex-col w-[19rem]'>
             <Typography variant='h6' gutterBottom>
               For Business Owners
             </Typography>

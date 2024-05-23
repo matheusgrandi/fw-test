d81eb5808e1c6331041d1ed59b421e8565293762
matheusgrandi <matheus@matheusgrandi.com.br>
2024-05-22 21:15:25 -0300

    fix: signup layout

--- src/components/organisms/signup/SignUpCard.tsx
+++ src/components/organisms/signup/SignUpCard.tsx
@@ -9,13 +9,13 @@ import DollarSignIcon from '../../atoms/icons/DollarSignIcon'
 
 const SignUpCard: React.FC = () => {
   return (
-    <Box className='h-full max-w-[60rem]'>
-      <Box className='flex items-center justify-center h-5/6 flex-col px-[calc(6.25rem-1.5rem)] gap-8'>
+    <Box className='h-full max-w-[60rem] '>
+      <Box className='flex items-center justify-center h-5/6 flex-col px-[calc(6.25rem-1.5rem)] gap-8 max-md:px'>
         <Typography variant='h2' gutterBottom className='self-end'>
           Fieldwork is the leading sales tool for your HVAC business.
         </Typography>
-        <Box className='flex gap-8 pr-[6.25rem] max-md:flex-col max-md: pr-[0]'>
-          <Box className='flex gap-6 flex-col w-[19rem] max-md:10'>
+        <Box className='flex gap-8 pr-[6.25rem] max-md:flex-col max-md:pr-[0]'>
+          <Box className='flex gap-6 flex-col w-[19rem]'>
             <Typography variant='h6' gutterBottom>
               For Business Owners
             </Typography>
